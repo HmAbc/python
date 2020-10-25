573c91302beccf146efee6b36764344545cede3d
HmAbc <houminghua3521@gmail.com>
2020-10-25 19:02:04 +0800

    modify user of commit

---
+++ sh.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+git filter-branch -f --env-filter '
+OLD_EMAIL="houminghua3521@163.com"
+CORRECT_NAME="HmAbc"
+CORRECT_EMAIL="houminghua3521@gmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
